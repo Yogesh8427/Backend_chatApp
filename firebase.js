60dc59e99d9ce95bf1438da005f9f59617324dba
Nikhilsarotri <nikhilc016@gmail.com>
2025-09-06 21:28:50 +0530

    1

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const dotenv= require("dotenv")
-const serviceAccount = require("./firebasekey.json");
+const serviceAccount = require("./utils/firebasekey.json");
 dotenv.config()
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
